1dd64acdfa57a0cd85695345f8de8ecacdfad3f9
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2021-06-09 21:40:00 +0200

    Leadando CRUDE feladathoz létrehozott adatbázishoz ORM osztályok létrehozása

---
+++ HajosTeszt/HajosTeszt/Models/Region.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HajosTeszt.Models
+{
+    public partial class Region
+    {
+        public Region()
+        {
+            Countries = new HashSet<Country>();
+        }
+
+        public int RegionId { get; set; }
+        public string RegionName { get; set; }
+
+        public virtual ICollection<Country> Countries { get; set; }
+    }
+}
