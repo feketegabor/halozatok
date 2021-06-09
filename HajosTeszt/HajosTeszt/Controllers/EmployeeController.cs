4141841f129f9c37054f38efd80b24122c445c28
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2021-06-09 23:33:19 +0200

    javítgatás

--- HajosTeszt/HajosTeszt/Controllers/EmployeeController.cs
+++ HajosTeszt/HajosTeszt/Controllers/EmployeeController.cs
@@ -14,12 +14,36 @@ namespace HajosTeszt.Controllers
     {
         [HttpGet]
         [Route("employee/count")]
-        public int M3()
+        public int M1()
         {
             CRUDE_dbContext context = new CRUDE_dbContext();
             var dolgozokSzama = (from x in context.Employees select x.EmployeeId).Count();
 
             return dolgozokSzama;
+        }
+
+        [HttpGet]
+        [Route("employee/all")]
+        public ActionResult M2()
+        {
+            CRUDE_dbContext context = new CRUDE_dbContext();
+            var dolgozok = from x in context.Employees select new { x.FirstName, x.LastName };
+
+            return new JsonResult(dolgozok);
+        }
+
+        [HttpGet]
+        [Route("employee/{id}")]
+        public ActionResult M3(int id)
+        {
+            CRUDE_dbContext context = new CRUDE_dbContext();
+            var dolgozo = (from x in context.Employees
+                          where x.EmployeeId == id
+                          select x).FirstOrDefault();
+
+            if (dolgozo == null) return BadRequest("Nincs ilyen ID-vel rendelkező dolgozó");
+
+            return new JsonResult(dolgozo);
         }
     }
 }
