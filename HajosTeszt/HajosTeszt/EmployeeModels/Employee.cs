cfee443320a5d7ee31cf7d5292abc1f173b850af
Gábor Fekete <gabor.fekete1@stud.uni-corvinus.hu>
2021-06-10 12:59:43 +0200

    Új adatbázis, api végpont összes rekordhoz

---
+++ HajosTeszt/HajosTeszt/EmployeeModels/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HajosTeszt.EmployeeModels
+{
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
